81f1809367805d7299cf5425a2013bdb94452482
Luis Fernando <fernu292@gmail.com>
2022-01-08 21:26:46 -0600

    Configurando Webpack

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
 
 const div = document.querySelector('#root');
 
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    resolve: { 
+        extensions: [".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, 
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            inject: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: "assets/[name].css"
+        })
+    ],
+    devServer : {
+        static: {
+            publicPath : path.join(__dirname, "dist")
+        },
+        port: 3000,
+        compress: true,
+        open: true
+    }
+}
